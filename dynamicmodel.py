cb56ab603e51bf3ea25bbcd485a4aad2dbed5fc6
Patrick O'Neil <poneil1@nd.edu>
2018-11-28 11:36:42 -0500

    adjusted LV for labels, RM adjusted but graph looks wrong

--- dynamicmodel.py
+++ dynamicmodel.py
@@ -15,7 +15,7 @@ times=range(0,100)
 params=(0.5,0.02,0.1,0.2)
 modelLV=spint.odeint(func=LV,y0=y0,t=times,args=params)
 modelLVDF=pandas.DataFrame({"t":times,"H":modelLV[:,0],"P":modelLV[:,1]})
-ggplot(modelLVDF,aes(x="t",y="H"))+geom_line()+geom_line(modelLVDF,aes(x="t",y="P"),color='red')+theme_classic()
+ggplot(modelLVDF,aes(x="t",y="H"))+geom_line()+geom_line(modelLVDF,aes(x="t",y="P"),color='red')+theme_classic()+ylab("Population size")+xlab("time")
 
 # Rosenzweig-MacArthur model
 
@@ -27,13 +27,15 @@ from plotnine import *
 def RM(y,t0,b,e,s,w,d,a):
 	H=y[0]
 	P=y[1]
-	dHdt=(b*H)(1-(a*H)-w*(H/(d+H))*P
-	dPdt=(e*w)(H/(d+H))*P-(s*P)
+	dHdt=(b*H)*(1-(a*H))-(w*(H/(d+H))*P)
+	dPdt=(e*w)*(H/(d+H))*P-(s*P)
 	return [dHdt,dPdt]
 times=range(0,100)
 y0=[500,120]
-params=(0.8,0.07,0.2,5,400)
+params=(0.8,0.07,0.2,5,400,0.001)
 modelRM=spint.odeint(func=RM,y0=y0,t=times,args=params)
 modelRMDF=pandas.DataFrame({"t":times,"H":RM[:,0],"P":RM[:,1]})
-ggplot(modelRMDF,aes(x="t",y="H"))+geom_line()+geom_line(modelRMDF,aes(x="t",y="P"),color='red')+theme_classic()
+ggplot(modelRMDF,aes(x="t",y="H"))+geom_line()+geom_line(modelRMDF,aes(x="t",y="P"),color='red')+theme_classic()+ylab("Population size")+xlab("time")
 
+
+ 
